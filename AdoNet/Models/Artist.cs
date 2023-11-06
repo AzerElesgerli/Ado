742c2cf326d1dd0a8cb76e4b4d2f4272c39b8daa
azer elesgerli <145683767+AzerElesgerli@users.noreply.github.com>
2023-11-06 11:41:22 +0400

    Task

---
+++ AdoNet/Models/Artist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNet.Models
+{
+    internal class Artist
+    {
+        public int ArtistId { get; set; }
+       
+        public string Surname { get; set; }
+        public string Name { get; set; }
+       
+        public string Gender { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ AdoNet/Program.cs
@@ -0,0 +1,63 @@
+﻿using AdoNet.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+namespace AdoNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string conn = "server=WINDOWS-JUPQ502;database=TasK ;trusted_connection=true;integrated security=true;";
+
+            SqlConnection connection = new SqlConnection(conn);
+            
+
+
+
+             void DeleteMusic()
+            {
+                connection.Open();
+                string SQL = "DELETE FROM Categories WHERE MusicId=1";
+            SqlCommand command = new SqlCommand(SQL, connection);
+                command.ExecuteNonQuery();
+
+            }
+            void CreateMusic()
+            {
+                connection.Open();
+                string Query = "INSERT INTO Musics (Name, Duration, CategoryId) VALUES (@Name, @Duration, @CategoryId)";
+                SqlCommand command = new SqlCommand(Query, connection);
+                command.ExecuteNonQuery();
+
+            }
+            void GetAll()
+            {
+                connection.Close();
+                string Query = "Select * From Musics";
+                SqlCommand command = new SqlCommand(Query, connection);
+                command.ExecuteNonQuery();
+
+
+            }
+             Musics GetMusicById(int id) 
+            {
+                connection.Close();
+
+                string query = "SELECT * FROM Musics WHERE MusicId = @MusicId";
+                SqlCommand command = new SqlCommand(query, connection);
+                foreach (DataRow row in )
+                Music music = new Music
+                         MusicId = (int)row["MusicId"],
+                         Name = row["Name"].ToString(),
+                         Duration = Convert.ToInt32(["Duration"]),
+                         CategoryId = (int)row["CategoryId"])
+
+
+            }
+
+
+        }
+    }
+}
